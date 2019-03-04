b3e99310775aa98973112099002d2353dcf77bd6
West Kraemer <kraemerwest@gmail.com>
2019-03-04 18:42:43 -0500

    override func of mammal

--- classes.playground/Contents.swift
+++ classes.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var str = "Hello, playground"
+
+class Mammal {
+    func sleep() {
+        print("sleeping")
+    }
+    func eat() {
+        print("eating")
+    } }
+let someMammal = Mammal()
+someMammal.eat()
+someMammal.sleep()
+
+
+class Bear: Mammal {
+    func hibernate() {
+        print("hibernating")
+    }
+    
+    override func eat() {
+        print("eating fish")
+    }
+}
+
+let aBear = Bear()
+aBear.eat()
