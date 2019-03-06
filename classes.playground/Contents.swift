a72bd40473688d55d121200d7e6c763890d33eb4
West Kraemer <kraemerwest@gmail.com>
2019-03-06 09:35:07 -0500

    add struct for separate initializers

--- classes.playground/Contents.swift
+++ classes.playground/Contents.swift
@@ -1,91 +1,108 @@
 import UIKit
+//
+//
+//class Mammal {
+//    func sleep() {
+//        print("sleeping")
+//    }
+//    func eat() {
+//        print("eating")
+//    }
+//}
+//
+//let someMammal = Mammal()
+//someMammal.eat()
+//someMammal.sleep()
+//
+//
+//class Bear: Mammal {
+//    var furColor = ""
+//
+//    func hibernate() {
+//        print("hibernating")
+//    }
+//
+//    override func eat() {
+//        var fish = "salmon"
+//        print("eating \(fish)!!")
+//    }
+//}
+//
+//let aBear = Bear()
+//aBear.eat()
+//print(aBear.hibernate())
+//
+////override init is how to change properties, whereas override func is how you change functions.
+//class PolarBear: Bear {
+//    override init() {
+//        super.init()
+//        furColor = "white"
+//    }
+//
+//    override func eat() {
+//        super.eat()
+//        var fish = "whale"
+//        print("eat \(fish)!")
+//
+//    }
+//}
+//
+//let anotherBear = PolarBear()
+//anotherBear.hibernate()
+//
+//anotherBear.eat()
+//
+//
+////optional binding
+//
+//var words:String?
+//
+//if let myString = words {
+//    print(myString)
+//} else {
+//    print("no string")
+//}
+//
+//
+//var myDictionary = ["key":"value", "k":"v", "ke":"va", "y":"ue"]
+//
+//if let firstKey = myDictionary["k"] {
+//    print(firstKey)
+//} else {
+//    print("no value")
+//}
+//
+//for _ in 0...5 {
+//    print("hello")
+//}
+//
+//for someVar in (1...6).reversed() {
+//    print(someVar)
+//}
+//
+//var array = ["New York", "New Jersey", "Montana"]
+//
+//for state in array {
+//    print(state)
+//}
+//
+//for arrIndex in 0...array.count-1 {
+//    print("\(arrIndex) is \(array[arrIndex])")
+//}
 
 
-class Mammal {
-    func sleep() {
-        print("sleeping")
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
     }
-    func eat() {
-        print("eating")
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
     }
 }
-
-let someMammal = Mammal()
-someMammal.eat()
-someMammal.sleep()
-
-
-class Bear: Mammal {
-    var furColor = ""
-    
-    func hibernate() {
-        print("hibernating")
-    }
-    
-    override func eat() {
-        var fish = "salmon"
-        print("eating \(fish)!!")
-    }
-}
-
-let aBear = Bear()
-aBear.eat()
-print(aBear.hibernate())
-
-//override init is how to change properties, whereas override func is how you change functions.
-class PolarBear: Bear {
-    override init() {
-        super.init()
-        furColor = "white"
-    }
-    
-    override func eat() {
-        super.eat()
-        var fish = "whale"
-        print("eat \(fish)!")
-        
-    }
-}
-
-let anotherBear = PolarBear()
-anotherBear.hibernate()
-
-anotherBear.eat()
-
-
-//optional binding
-
-var words:String?
-
-if let myString = words {
-    print(myString)
-} else {
-    print("no string")
-}
-
-
-var myDictionary = ["key":"value", "k":"v", "ke":"va", "y":"ue"]
-
-if let firstKey = myDictionary["k"] {
-    print(firstKey)
-} else {
-    print("no value")
-}
-
-for _ in 0...5 {
-    print("hello")
-}
-
-for someVar in (1...6).reversed() {
-    print(someVar)
-}
-
-var array = ["New York", "New Jersey", "Montana"]
-
-for state in array {
-    print(state)
-}
-
-for arrIndex in 0...array.count-1 {
-    print("\(arrIndex) is \(array[arrIndex])")
-}
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+// boilingPointOfWater.temperatureInCelsius is 100.0
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+// freezingPointOfWater.temperatureInCelsius is 0.0
+print(boilingPointOfWater)
+print(freezingPointOfWater)
