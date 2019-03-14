f10b87278bcbaea62e1c84a869b75c289c742fa1
West Kraemer <kraemerwest@gmail.com>
2019-03-14 14:08:45 -0400

    add funcs

--- randoQuiz.playground/Contents.swift
+++ randoQuiz.playground/Contents.swift
@@ -49,3 +49,51 @@ let j = i + i
 let num = UInt.min
 
 
+var optionalName: String? = nil
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+} else {
+    print("name was nil")
+}
+
+let nickName: String? = "West"
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickName ?? fullName)"
+
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 60, 1, 2, 3, 5, 8],
+    "Square": [50, 1, 4, 9, 16, 25],
+]
+var largest = 0
+var largestKind = ""
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            largestKind = kind
+        }
+    }
+}
+print(largest)
+print(largestKind)
+
+func greet(_ person: String, _ lunchSpecial: String) -> String {
+    return "Hey, \(person), this is the lunch special \(lunchSpecial)"
+}
+print(greet("west", "carrots"))
